e4dc44e5d09e27db2e89a44ed0e9a0c0fc81bf13
shaohuali <shaohuali@bestechnic.com>
2019-10-31 17:28:23 +0800

    pod.spec config

---
+++ UsingCoreBluetoothClassic5/CoreBluetoothClassicSample/AppDelegate.swift
@@ -0,0 +1,14 @@
+/*
+See LICENSE folder for this sample’s licensing information.
+
+Abstract:
+The AppDelegate to handle system events.
+*/
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+}
+
